fd08b8261abb3f871146c963902e81057f79b1e2
alexey-mr <alexey.morlang@gmail.com>
2021-02-18 00:42:17 +0300

    Dont reload cassandra sts on config nodes change
    - reload only nodemgr as noly it needs this info
    
    Change-Id: If8d52017cd35c4dd0034acacedcc932861176fe4

--- pkg/apis/contrail/v1alpha1/templates/nodemanager_config.go
+++ pkg/apis/contrail/v1alpha1/templates/nodemanager_config.go
@@ -7,8 +7,16 @@ import "text/template"
 // to commonize approach with sighup between orchestrators
 var NodemanagerRunner = template.Must(template.New("").Parse(`#!/bin/bash
 [[ "$LOG_LEVEL" != "SYS_DEBUG" ]] || set -x
-cfg_file=/etc/contrailconfigmaps/${NODE_TYPE}-nodemanager.conf.${POD_IP}
-vnc_file=/etc/contrailconfigmaps/vnc_api_lib.ini.${POD_IP}
+function link_file() {
+  local src=/etc/contrailconfigmaps/$1
+  local dst=/etc/contrail/${2:-${1}}
+  echo INFO: $(date): wait for $src
+  while [ ! -e $src ] ; do sleep 1; done
+  echo INFO: $(date): link $src => $dst
+  ln -sf $src $dst
+	cat $dst
+}
+
 sig_file=/var/run/nodemanager-${NODE_TYPE}-${POD_IP}.pid.sighup
 pid_file=/var/run/nodemanager-${NODE_TYPE}-${POD_IP}.pid
 trap 'trap_sighup' SIGHUP
@@ -30,21 +38,12 @@ function trap_sighup() {
 touch $sig_file
 while [ -e $sig_file ] ; do
 	rm -f $sig_file $pid_file
-	
-	echo "INFO: $(date): wait for config $cfg_file"
-	while [ ! -e $cfg_file ] ; do sleep 5 ; done
-	echo "INFO: $(date): got config:"
-	cat $cfg_file
-	echo ""
-	echo "INFO: $(date): wait for vnc config $vnc_file"
-	while [ ! -e $vnc_file ] ; do sleep 5 ; done
-	echo "INFO: $(date): got vnc config"
-	cat $vnc_file
-	echo ""
-	echo "INFO: $(date): link configs to /etc/contrail"
-	ln -sf $cfg_file /etc/contrail/contrail-${NODE_TYPE}-nodemgr.conf
-	ln -sf $vnc_file /etc/contrail/vnc_api_lib.ini
 
+	link_file ${NODE_TYPE}-nodemgr.env.${POD_IP} ${NODE_TYPE}-nodemgr.env
+	link_file vnc_api_lib.ini.${POD_IP} vnc_api_lib.ini
+	link_file ${NODE_TYPE}-nodemgr.conf.${POD_IP} contrail-${NODE_TYPE}-nodemgr.conf
+	
+	source /etc/contrail/${NODE_TYPE}-nodemgr.env
 	/usr/bin/contrail-nodemgr --nodetype=contrail-${NODE_TYPE} &
 	pid=$!
 	echo $pid > $pid_file
@@ -52,4 +51,10 @@ while [ -e $sig_file ] ; do
 	wait $pid
 done
 
+`))
+
+// NodemanagerEnv env for nodemgr
+var NodemanagerEnv = template.Must(template.New("").Parse(`
+export ANALYTICSDB_NODES={{ .AnalyticsDBNodes }}
+export CONFIGDB_NODES={{ .ConfigDBNodes }}
 `))
