4946352e86e41223c87dd97746ef2c1794e91be3
Kirill Egorov <kegorov@progmaticlab.com>
2021-05-31 18:28:14 +0300

    Add label to updated statefulsets recreate its pods
    Change-Id: I92afb1cf6cd741fd390c07c7b462aa686c78f27c
    Signed-off-by: Kirill Egorov <kegorov@progmaticlab.com>
    (cherry picked from commit 31490e41d25836b9c231a859db875a016192130a)

--- pkg/apis/tf/v1alpha1/base_types.go
+++ pkg/apis/tf/v1alpha1/base_types.go
@@ -15,6 +15,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -325,7 +326,7 @@ type PodAlternativeIPs struct {
 func PodsCertSubjects(domain string, podList []corev1.Pod, podAltIPs PodAlternativeIPs) []certificates.CertificateSubject {
 	var pods []certificates.CertificateSubject
 	for _, pod := range podList {
-        hostname := pod.Spec.NodeName
+		hostname := pod.Spec.NodeName
 		var alternativeIPs []string
 		if podAltIPs.ServiceIP != "" {
 			alternativeIPs = append(alternativeIPs, podAltIPs.ServiceIP)
@@ -650,6 +651,7 @@ func UpdateSTS(name string,
 		logger.Info("Some of container images or env changed, or force mode")
 		changed = true
 		sts.Spec.Template = *template
+		sts.Spec.Template.Labels["change-at"] = time.Now().Format("2006-01-02-15-04-05")
 	}
 
 	if replicas != nil && *replicas != *sts.Spec.Replicas {
