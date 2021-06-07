ad20a159db976169e6504465feba1a5b8e493b3f
Alexey <alexey.morlang@gmail.com>
2021-06-08 01:10:33 +0300

    Refactore client set to allow overwrite for unittests
    Change-Id: Icd51b0b56cac25649e7c925a510626804b811eaa

--- pkg/k8s/client.go
+++ pkg/k8s/client.go
@@ -5,133 +5,61 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/user"
 	"path/filepath"
+	"sync"
 
 	core_v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
+	corev1api "k8s.io/client-go/kubernetes/typed/core/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/remotecommand"
 )
 
-// GetConfig creates a *rest.Config for talking to a Kubernetes apiserver.
-// If --kubeconfig is set, will use the kubeconfig file at that location.  Otherwise will assume running
-// in cluster and use the cluster provided kubeconfig.
-//
-// Config precedence
-//
-// * --kubeconfig flag pointing at a file
-//
-// * KUBECONFIG environment variable pointing at a file
-//
-// * In-cluster config if running in cluster
-//
-// * $HOME/.kube/config if exists
-func GetConfig() (*rest.Config, error) {
-	var kubeconfig, masterURL string
-	// If a flag is specified with the config location, use that
-	if len(kubeconfig) > 0 {
-		return clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-	}
-	// If an env variable is specified with the config locaiton, use that
-	if len(os.Getenv("KUBECONFIG")) > 0 {
-		return clientcmd.BuildConfigFromFlags(masterURL, os.Getenv("KUBECONFIG"))
-	}
-	// If no explicit location, try the in-cluster config
-	if c, err := rest.InClusterConfig(); err == nil {
-		return c, nil
-	}
-	// If no in-cluster config, try the default location in the user's home directory
-	if usr, err := user.Current(); err == nil {
-		if c, err := clientcmd.BuildConfigFromFlags(
-			"", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
-			return c, nil
-		}
-	}
+var lock = sync.Mutex{}
 
-	return nil, fmt.Errorf("could not locate a kubeconfig")
+// var clientset *kubernetes.Clientset
+var coreAPI corev1api.CoreV1Interface
+
+// Allows to overwrite clientset for unittests
+func SetClientset(c corev1api.CoreV1Interface) {
+	lock.Lock()
+	defer lock.Unlock()
+	coreAPI = c
+	// clientset = c
 }
 
 // GetClientConfig first tries to get a config object which uses the service account kubernetes gives to pods,
 // if it is called from a process running in a kubernetes environment.
 // Otherwise, it tries to build config from a default kubeconfig filepath if it fails, it fallback to the default config.
 // Once it get the config, it returns the same.
-func GetClientConfig() (*rest.Config, error) {
+func getClientConfig() *rest.Config {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		err1 := err
 		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
 		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
 		if err != nil {
-			err = fmt.Errorf("InClusterConfig as well as BuildConfigFromFlags Failed. Error in InClusterConfig: %+v\nError in BuildConfigFromFlags: %+v", err1, err)
-			return nil, err
+			panic(fmt.Errorf("InClusterConfig as well as BuildConfigFromFlags Failed. Error in InClusterConfig: %+v\nError in BuildConfigFromFlags: %+v", err1, err))
 		}
 	}
-
-	return config, nil
+	return config
 }
 
-// GetClientsetFromConfig takes REST config and Create a clientset based on that and return that clientset.
-func GetClientsetFromConfig(config *rest.Config) (*kubernetes.Clientset, error) {
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		err = fmt.Errorf("failed creating clientset. Error: %+v", err)
-		return nil, err
-	}
-
-	return clientset, nil
-}
-
-// GetClientset first tries to get a config object which uses the service account kubernetes gives to pods,
+// GetCoreV1 first tries to get a config object which uses the service account kubernetes gives to pods,
 // if it is called from a process running in a kubernetes environment.
 // Otherwise, it tries to build config from a default kubeconfig filepath if it fails, it fallback to the default config.
 // Once it get the config, it creates a new Clientset for the given config and returns the clientset.
-func GetClientset() (*kubernetes.Clientset, error) {
-	config, err := GetClientConfig()
-	if err != nil {
-		return nil, err
+func GetCoreV1() corev1api.CoreV1Interface {
+	lock.Lock()
+	defer lock.Unlock()
+	if coreAPI == nil {
+		clientset := kubernetes.NewForConfigOrDie(getClientConfig())
+		coreAPI = clientset.CoreV1()
 	}
-
-	return GetClientsetFromConfig(config)
-}
-
-// GetDynamicClientFromConfig takes REST config and Create a dynamic client based on that return that client.
-func GetDynamicClientFromConfig(config *rest.Config) (dynamic.Interface, error) {
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	return dynamicClient, nil
-}
-
-// GetDynamicClient first tries to get REST config object which uses the service account kubernetes gives to pods,
-// if it is called from a process running in a kubernetes environment.
-// Otherwise, it tries to build config from a default kubeconfig filepath if it fails, it fallback to the default config.
-// Once it get the config, it creates a new Dynamic Client for the given config and returns the client.
-func GetDynamicClient() (dynamic.Interface, error) {
-	config, err := GetClientConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return GetDynamicClientFromConfig(config)
-}
-
-// GetRESTClient first tries to get a config object which uses the service account kubernetes gives to pods,
-// if it is called from a process running in a kubernetes environment.
-// Otherwise, it tries to build config from a default kubeconfig filepath if it fails, it fallback to the default config.
-// Once it get the config, it
-func GetRESTClient() (*rest.RESTClient, error) {
-	config, err := GetClientConfig()
-	if err != nil {
-		return &rest.RESTClient{}, err
-	}
-
-	return rest.RESTClientFor(config)
+	return coreAPI
 }
 
 // ExecToPodThroughAPI uninterractively exec to the pod with the command specified.
@@ -143,17 +71,7 @@ func GetRESTClient() (*rest.RESTClient, error) {
 //          string: Errors. (STDERR)
 //           error: If any error has occurred otherwise `nil`
 func ExecToPodThroughAPI(command []string, containerName, podName, namespace string, stdin io.Reader) (string, string, error) {
-	config, err := GetClientConfig()
-	if err != nil {
-		return "", "", err
-	}
-
-	clientset, err := GetClientsetFromConfig(config)
-	if err != nil {
-		return "", "", err
-	}
-
-	req := clientset.CoreV1().RESTClient().Post().
+	req := GetCoreV1().RESTClient().Post().
 		Resource("pods").
 		Name(podName).
 		Namespace(namespace).
@@ -174,7 +92,7 @@ func ExecToPodThroughAPI(command []string, containerName, podName, namespace str
 		TTY:       false,
 	}, parameterCodec)
 
-	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	exec, err := remotecommand.NewSPDYExecutor(getClientConfig(), "POST", req.URL())
 	if err != nil {
 		return "", "", fmt.Errorf("error while creating Executor: %v", err)
 	}
