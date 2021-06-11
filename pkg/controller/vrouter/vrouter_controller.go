ca7a3b55405839ffea4b1051981385485a1a29b0
Alexey <alexey.morlang@gmail.com>
2021-06-11 18:13:28 +0300

    added watch analitycs for vrouter
    Change-Id: I70b6df69794c4019fa5f64c8653338f7beba84a7

--- pkg/controller/vrouter/vrouter_controller.go
+++ pkg/controller/vrouter/vrouter_controller.go
@@ -151,6 +151,13 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
+	srcAnalytics := &source.Kind{Type: &v1alpha1.Analytics{}}
+	analyticsHandler := resourceHandler(mgr.GetClient())
+	predAnalyticsSizeChange := utils.AnalyticsActiveChange()
+	if err = c.Watch(srcAnalytics, analyticsHandler, predAnalyticsSizeChange); err != nil {
+		return err
+	}
+
 	srcDS := &source.Kind{Type: &appsv1.DaemonSet{}}
 	dsHandler := &handler.EnqueueRequestForOwner{
 		IsController: true,
