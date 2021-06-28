9f9a3b1b0c9f0dcf035297703b966500b2081a85
Alexey <alexey.morlang@gmail.com>
2021-06-28 14:54:01 +0300

    Set priority class name
    - to prevent evictiona and oom kill
    (for node critical kubernetes sets oom_score_adj to -997
    that makes it lower proirity for oomkiller)
    
    Change-Id: Ia4c3afdc2d53bc9bdfc610e93cf88ddcb7c275ff
    closes-jira-bug: CEM-22062

--- pkg/apis/tf/v1alpha1/base_types.go
+++ pkg/apis/tf/v1alpha1/base_types.go
@@ -1783,6 +1783,8 @@ func DefaultSecurityContext(podSpec *corev1.PodSpec) {
 			c.SecurityContext.RunAsGroup = &rootid
 		}
 	}
+	// to prevent PODs to be evicted or OOM killed
+	podSpec.PriorityClassName = "system-node-critical"
 }
 
 // IsOKForRequeque works for errors from request for update, and returns true if
