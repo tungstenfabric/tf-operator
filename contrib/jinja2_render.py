e6e5b0b847e9f5fc9607abae9707ae876bdb0054
Alexey <alexey.morlang@gmail.com>
2022-05-25 17:37:44 +0300

    simplify passing ca params
    - to avoid manual base64 encoding by user
    
    closes-jira-bug: CEM-27234
    Change-Id: If7f1d5cd42da82ab3342540c3184e0e12721a446

--- contrib/jinja2_render.py
+++ contrib/jinja2_render.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import base64
 import jinja2
 import os
 import sys
 
-template = jinja2.Template(sys.stdin.read())
+def to_bytes(v):
+  return v if isinstance(v, bytes) else v.encode()
+
+def b64encode(v):
+  return base64.b64encode(to_bytes(v)).decode(encoding='utf-8')
+
+def b64decode(v):
+  return base64.b64decode(v).decode(encoding='utf-8')
+
+env = jinja2.Environment()
+env.filters['b64encode'] = b64encode
+env.filters['b64decode'] = b64decode
+template = env.from_string(sys.stdin.read())
 print(template.render(**os.environ))
